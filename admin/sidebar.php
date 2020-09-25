98987b9d60facd3a04973f6950753fe11b92df08
Jahirul Islam <jahirul.iit5th@gmail.com>
2020-09-25 19:41:31 +0600

    sidebar change
    change link

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,83 +1,75 @@
  <aside class="main-sidebar">
-							 
+
 								<section class="sidebar">
-								 
+
 								  <ul class="sidebar-menu">
 									<li class="header">MAIN NAVIGATION</li>
 									<li class="active treeview">
-									  <a href="http://localhost/smart_payroll/admin/dashboard.php">
-										<i class="fa fa-dashboard"></i> <span>Dashboard</span> 
+									  <a href="dashboard.php">
+										<i class="fa fa-dashboard"></i> <span>Dashboard</span>
 									  </a>
-									 
+
 									</li>
-            
+
 														<li>
-														  <a href="http://localhost/smart_payroll/admin/edit_admin.php">
-															<i class="fa fa-flag"></i> <span>My Profile</span> 
+														  <a href="edit_admin.php">
+															<i class="fa fa-flag"></i> <span>My Profile</span>
 														  </a>
 														</li>
-			
-			
-			
-								<li ><a href="http://localhost/smart_payroll/admin/employee/add_employee.php"><i class="fa fa-user"></i> Add Employee</a></li>
-										<li><a href="http://localhost/smart_payroll/admin/employee/all_employee.php"><i class="fa fa-user"></i> All Employee</a></li>
-											<li><a href="http://localhost/smart_payroll/admin/employee/view_all.php"><i class="fa fa-user"></i> View Attendance</a></li>
-								
-								
-								
-								
+
+
+
+								<li ><a href="add_employee.php"><i class="fa fa-user"></i><span> Add Employee</span></a></li>
+										<li><a href="all_employee.php"><i class="fa fa-user"></i><span> All Employee</span></a></li>
+											<li><a href="view_all.php"><i class="fa fa-user"></i><span> View Attendance</span></a></li>
+
+
+
+
 									<li>
-										  <a href="http://localhost/smart_payroll/admin/department/department.php">
-											<i class="fa fa-th"></i> <span>Department</span> 
+										  <a href="department.php">
+											<i class="fa fa-th"></i> <span>Department</span>
 										  </a>
 										</li>
 										<li>
-			
-			
-										
-										
+
+
+
+
 										<li>
-										  	  <a href="http://localhost/smart_payroll/admin/task/all_employee.php">
-											<i class="fa fa-th"></i> <span>Task management</span> 
+										  	  <a href="all_employee.php">
+											<i class="fa fa-th"></i> <span>Task management</span>
 										  </a>
 										</li>
 										<li>
-										  	   <a href="http://localhost/smart_payroll/admin/task/all_employee_task.php">
-											<i class="fa fa-th"></i> <span>Performance</span> 
+										  	   <a href="all_employee_task.php">
+											<i class="fa fa-th"></i> <span>Performance</span>
 										  </a>
 										</li>
-										
-								
+
+
 			                 <li>
-								  <a href="http://localhost/smart_payroll/admin/files/files.php">
-									<i class="fa fa-book"></i> <span>All Files</span> 
+								  <a href="files.php">
+									<i class="fa fa-book"></i> <span>All Files</span>
 								  </a>
 								</li>
-		
+
                            <li>
-								  <a href="http://localhost/smart_payroll/admin/message.php">
-									<i class="fa fa-th"></i> <span>Messages</span> 
+								  <a href="message.php">
+									<i class="fa fa-th"></i> <span>Messages</span>
 								  </a>
 								</li>
-								
-								 
-            
-           
-            
+
+
+
+
+
 								<li>
-								  <a href="http://localhost/smart_payroll/admin/event.php">
+								  <a href="event.php">
 									<i class="fa fa-calendar"></i> <span>Events</span>
-									
+
 								  </a>
 								</li>
-            
-            
-            
-            
-            <li class="header">LABELS</li>
-            
-            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
-          </ul>
         </section>
         <!-- /.sidebar -->
       </aside>
